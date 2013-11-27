716634855a2f108c17bfa36450cf3ecfcb3e4afb
hunt burd <hunt@websitescenes.com>
2013-11-27 02:58:46 -0700

    updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,5 +7,9 @@ $(document).ready(function(){
 		$('.sale_line_items').append("<div class='row'><div class='col-sm-2'><label>Sku</label><input class='form-control' id='sale_items_attributes_" + row + "_sku' name='sale[items_attributes][" + row + "][sku]' placeholder='SKU' type='text' /></div><div class='col-sm-3'><label>Item</label><input class='form-control' id='sale_items_attributes_" + row + "_name' name='sale[items_attributes][" + row + "][name]' placeholder='Item' type='text' /></div><div class='col-sm-3'><label>Description</label><input class='form-control' id='sale_items_attributes_" + row + "_description' name='sale[items_attributes][" + row + "][description]' placeholder='Description' type='text' /></div><div class='col-sm-2'><label>Price</label><input class='form-control' id='sale_items_attributes_" + row + "_price' name='sale[items_attributes][" + row + "][price]' placeholder='Price' type='text' /></div><div class='col-sm-2'><label>Quantity</label><input class='form-control' id='sale_items_attributes_" + row + "_price' name='sale[items_attributes][" + row + "][price]' placeholder='Quantity' type='text' /></div></div>");
 	});
 
+	$('.item_search_input').on("keypress", function(){
+    $('.item_search').click();
+  });
+
 
 });
