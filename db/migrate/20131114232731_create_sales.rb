296c9d3dfde6e94931b88df633869d54c20bb8de
hunt burd <hunt@websitescenes.com>
2013-12-03 21:13:22 -0700

    checking in

--- db/migrate/20131114232731_create_sales.rb
+++ db/migrate/20131114232731_create_sales.rb
@@ -1,8 +1,11 @@
 class CreateSales < ActiveRecord::Migration
   def change
     create_table :sales do |t|
-    	t.decimal :total_amount, :precision => 8, :scale => 2
+    	t.decimal :amount, :precision => 8, :scale => 2
+      t.decimal :total_amount, :precision => 8, :scale => 2
       t.decimal :remaining_amount
+      t.decimal :discount, :precision => 6, :scale => 5
+      t.decimal :tax, :precision => 6, :scale => 5
       t.integer :customer_id
       t.text :comments
 
