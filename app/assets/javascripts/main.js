15b5f68c492a9cbe23e563a6cb5b113b0dfde410
hunt burd <hunt@websitescenes.com>
2013-12-09 00:26:11 -0700

    logs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 
 	$('.item_search_input').on("keypress", function(){
-	    $('.item_search').click();
-	  });
+	  $('.item_search').click();
+	});
+
+	$('#sale_discount').on("change", function(){
+	  $('.sale_submit').click();
+	});	
+
+	$('.price_control').on("change", function(){
+	  $('.sale_submit').click();
+	});	
 
 
 });
