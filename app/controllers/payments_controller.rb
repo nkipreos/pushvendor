92b97f494eaadc56e850efb6e663a134f102154c
hunt burd <hunt@websitescenes.com>
2013-12-14 16:39:46 -0700

    checking in.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,8 @@
 class PaymentsController < ApplicationController
 
 	def make_payment
+		@sale = Sale.find(params[:payments][:sale_id])
+		payment = Payment.create(:payment_type => params[:payments][:payment_type], :amount => params[:payments][:amount], :sale_id => params[:payments][:sale_id])
 
   	respond_to do |format|
       format.js
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,5 @@
 class Payment < ActiveRecord::Base
+	attr_accessible :payment_type, :amount, :sale_id
+
 	belongs_to :sale
 end
