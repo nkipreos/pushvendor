ca83e66ec6c6a9a7f6be84faf4003d6fc2ba72c2
Hunt Burdick <hunt@impulsedevelopment.com>
2014-04-07 14:04:58 -0600

    cleaning up code base.

--- app/views/sales/create_custom_item.js
+++
@@ -1,4 +0,0 @@
-$('#customItem').modal('hide');
-$('.modal-open').removeClass('modal-open');
-$('.modal-backdrop').removeClass('modal-backdrop');
-$('.sale_form_details').html("<%= escape_javascript render(:file => 'sales/_form.html.erb') %>");
