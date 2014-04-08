b3cb723cd9c44cafb84bf1b1c96fe873fc608cb4
Hunt Burdick <hunt@impulsedevelopment.com>
2014-04-08 12:59:11 -0600

    javascript updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :can_update_users, :can_update_items, :can_update_configuration, :can_view_reports, :can_update_sale_discount, :can_remove_sales
+
+  validates :username, :presence => true, :uniqueness => true
+
 end
