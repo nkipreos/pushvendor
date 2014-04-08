40a65103832630e57ce926174de1cd82ecbc3b52
Hunt Burdick <hunt@impulsedevelopment.com>
2014-04-08 11:01:30 -0600

    username for login instead of email address.

--- db/migrate/20131114173358_devise_create_users.rb
+++ db/migrate/20131114173358_devise_create_users.rb
@@ -2,12 +2,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :can_update_users => true, :can_update_items => true, :can_update_configuration => true, :can_view_reports => true, :can_update_sale_discount => true, :can_remove_sales => true) if direction == :up
+    User.create!(:email => 'admin@example.com', :username => 'admin', :password => 'password', :password_confirmation => 'password', :can_update_users => true, :can_update_items => true, :can_update_configuration => true, :can_view_reports => true, :can_update_sale_discount => true, :can_remove_sales => true) if direction == :up
   end
 
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :username,           :null => false, :default => ""
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
@@ -52,6 +53,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
+    add_index :users, :username,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20131205212936) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "username",                 default: "",    null: false
     t.string   "email",                    default: "",    null: false
     t.string   "encrypted_password",       default: "",    null: false
     t.string   "reset_password_token"
@@ -109,5 +110,6 @@ ActiveRecord::Schema.define(version: 20131205212936) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
