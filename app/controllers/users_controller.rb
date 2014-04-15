373082ba86a8b175f78ea59b57c911364c49d1f0
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-04-15 13:09:46 -0600

    breadcrumbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,10 +75,14 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    if @user.id == 1
+      redirect_to users_url, notice: "You can't delete the main administrator!"
+    else
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
     end
   end
 
