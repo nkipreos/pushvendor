7bd6b3eb10ebdfd9b03ac65bb6e75428db90eaac
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-04-15 13:47:26 -0600

    just checking in.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
   	@recent_sales = Sale.find(:all, :limit => 10, :order => 'id DESC')
+  	@popular_items = Item.find(:all, :limit => 10, :order => 'amount_sold DESC')
   end
 end
