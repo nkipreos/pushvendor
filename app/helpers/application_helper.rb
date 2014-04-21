085f88cdffc2d9b93005b74f5cf432c84152c522
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-04-21 14:11:09 -0600

    fix front page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,5 +31,12 @@ module ApplicationHelper
 		
 	end
 
+	def get_http(url)
+    unless url.blank? || url.starts_with?("http://") || url.starts_with?("https://")
+      url = "http://" + url
+    end
+    url
+  end
+
 
 end
