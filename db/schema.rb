507ed8e912bddc34159de3387f7f6b04872830ca
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-09-11 16:29:54 -0600

    include default database.yml file and use sqlite3 by default.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140420165735) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "customers", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -119,8 +116,8 @@ ActiveRecord::Schema.define(version: 20140420165735) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
