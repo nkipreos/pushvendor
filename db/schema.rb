2cba95d5a1c63d2bd294188e065065a5d02cf46e
Nicolas Kipreos <nkipreos@gmail.com>
2016-02-01 22:27:41 -0300

    change datatype in expenses amount

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201195159) do
+ActiveRecord::Schema.define(version: 20160202012152) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name",    limit: 255
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20160201195159) do
   create_table "expenses", force: :cascade do |t|
     t.string   "name",            limit: 255
     t.text     "description",     limit: 65535
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "amount",          limit: 4
+    t.datetime "created_at",                                            null: false
+    t.datetime "updated_at",                                            null: false
+    t.decimal  "amount",                        precision: 8, scale: 2
     t.integer  "money_source_id", limit: 4
   end
 
